5c864623441e5431978f513705bbd2adec6a6c5e
oyin-k <oyink7@gmail.com>
2020-04-24 19:09:25 +0100

    SearchParams component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Oby" animal="Dog" breed="German Shephard" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/useDropdown.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const useDropdown = (label, defaultState, options) => {
+  const [state, setState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown = () => {
+    <label htmlFor={id}>
+      {label}
+      <select
+        id={id}
+        value={state}
+        onChange={(e) => setState(e.target.value)}
+        onBlur={(e) => setState(e.target.value)}
+        disabled={options.length === 0}
+      ></select>
+    </label>;
+  };
+};
+
+export default useDropdown;
